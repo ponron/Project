9a9a666f676dcfb37d253844b266c6e4151ca0ea
my-web-site <ponron555@gmail.com>
2024-09-18 01:58:51 +0700

    frist commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const progressBar = document.getElementById('progressBar');
-    const totalDuration = 5000; // Total duration of progress (in milliseconds)
+    const totalDuration = 3000; // Total duration of progress (in milliseconds)
     const interval = 100; // Interval for updating the progress (in milliseconds)
     
     let startTime = null;
